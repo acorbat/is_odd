83ee467b2029290634b0080221215f3971eea066
acorbat <agustin.corbat@ki.se>
2023-05-30 14:17:08 +0200

    renamed function to is_odd and swapped mods and tests

--- src/is_even.py
+++ src/is_even.py
@@ -1,34 +1,34 @@
-def is_even(x):
-    """Returns True if x is even
+def is_odd(x):
+    """Returns True if x is odd
 
     Parameters
     ----------
     x: int
-        integer to check if it is even.
+        integer to check if it is odd.
 
     Returns
     -------
     : bool
-        Returns True if x is even, else it returns False"""
+        Returns True if x is odd, else it returns False"""
     if not isinstance(x, int):
         raise TypeError('x must be an integer')
 
-    if x % 2 == 0:
+    if x % 2 == 1:
         return True
     else:
         return False
 
 
-assert is_even.__doc__ is not None
+assert is_odd.__doc__ is not None
 
 raised_typeerror = False
 try:
-    is_even('das')
+    is_odd('das')
 except TypeError:
     raised_typeerror = True
 
 assert raised_typeerror
 
-assert is_even(2)
+assert is_odd(2) is False
 
-assert is_even(1) is False
+assert is_odd(1)
