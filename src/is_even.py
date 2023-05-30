156f9f53e2557b59795907c9e8c7285fb8a9bad3
acorbat <agustin.corbat@ki.se>
2023-05-30 13:40:15 +0200

    Add Documentation for is_even

---
+++ src/is_even.py
@@ -0,0 +1,15 @@
+def is_even(x):
+    """Returns True if x is even
+
+    Parameters
+    ----------
+    x: int
+        integer to check if it is even.
+
+    Returns
+    -------
+    : bool
+        Returns True if x is even, else it returns False"""
+    pass
+
+assert is_even.__doc__ is not None
