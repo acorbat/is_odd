4a5638d06601eef46b692da43308a813112c11c8
acorbat <agustin.corbat@ki.se>
2023-05-30 13:44:02 +0200

    is_even checks input type is integer

--- src/is_even.py
+++ src/is_even.py
@@ -11,5 +11,16 @@ def is_even(x):
     : bool
         Returns True if x is even, else it returns False"""
     pass
+    if not isinstance(x, int):
+        raise TypeError('x must be an integer')
+
 
 assert is_even.__doc__ is not None
+
+raised_typeerror = False
+try:
+    is_even('das')
+except TypeError:
+    raised_typeerror = True
+
+assert raised_typeerror
